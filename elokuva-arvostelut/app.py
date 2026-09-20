0320a31da9e57abb21784926886cddda322692ec
Verónika Reznichenko <veronikareznichenko@Veronika-MacBook-Air.local>
2026-09-20 22:29:19 +0300

    Add new item form

--- elokuva-arvostelut/app.py
+++ elokuva-arvostelut/app.py
@@ -13,6 +13,21 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    review = request.form["review"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO reviews (title, review, user_id) VALUES (?, ?, ?)"""
+    db.execute(sql, [title, review, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -42,9 +57,12 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
             
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -53,4 +71,5 @@ def login():
 @app.route("/logout")
 def logout():
     del session["username"]
+    del session["user_id"]
     return redirect("/")
---
+++ elokuva-arvostelut/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
+
+CREATE TABLE reviews (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    review TEXT,
+    user_id INTEGER REFERENCES users
+);
